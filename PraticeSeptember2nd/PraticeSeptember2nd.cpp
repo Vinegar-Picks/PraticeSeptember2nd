3f8c35a23f354598f63765ca4f9c5fe635dcb2ec
Vinegar-Picks <ymancilla0623@gmail.com>
2026-09-02 19:57:16 -0500

    Add project files.

---
+++ PraticeSeptember2nd/PraticeSeptember2nd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//#include <iostream> makes cout available.
+using namespace std;
+//using namespace std; lets you write cout instead of std::cout.
+
+int main()
+//int main() is where the program starts.
+{
+	// cout shows information, program talks to you // cin receives information, program listens to you
+    cout << "Yovani's First Practice Run\n";
+    //cout << "Yovani's First Practice Run\n"; is the line that actually produces visible output.
+    int age = 20;
+    age = age + 10;
+	cout << "My age is ==> " << age << endl;
+}
